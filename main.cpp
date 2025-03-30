0004a8b8adc6f4f7647e25004d5ea89ca8154eec
Myrzabek <mlgexgood@gmail.com>
2025-03-30 18:40:53 +0500

    project init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+char *screen = new char[WIDTH * HEIGHT];
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
